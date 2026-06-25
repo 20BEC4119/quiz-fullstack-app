1af6283474f2a1e7e15f07eb201dbc7597bf6c59
Naveen <naveenm11052003@gmail.com>
2026-06-25 21:08:18 +0530

    Connect Railway MySQL

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQLDATABASE
 });
-
 db.connect((err) => {
     if (err) {
         console.log(err);
