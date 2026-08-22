94d06950c110dcf1f9f1edf60dbee15d9d61701e
Naveen <naveenm11052003@gmail.com>
2026-08-22 22:25:56 +0530

    Serve frontend files

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const User = require("./models/user");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 
 // ======================================
